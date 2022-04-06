27fb3761dd7dfb257c333b9e8a4b08e781533b64
henrikBjorserud <henrikbjorserud@gmail.com>
2022-04-06 13:52:14 +0200

    Fixed crop

--- pcb_anodet_server/predict.py
+++ pcb_anodet_server/predict.py
@@ -7,18 +7,24 @@ import cv2
 
 def get_predict_model(path):
     model_data_path = path + "/distributions"
-    mean = torch.load(os.path.join(model_data_path, 'mean.pt'))
-    cov_inv = torch.load(os.path.join(model_data_path, 'cov_inv.pt'))
-    model = anodet.Padim(backbone='resnet18', mean=mean, cov_inv=cov_inv, device=torch.device('cuda'))
+    mean = torch.load(os.path.join(model_data_path, "mean.pt"))
+    cov_inv = torch.load(os.path.join(model_data_path, "cov_inv.pt"))
+    model = anodet.Padim(
+        backbone="resnet18", mean=mean, cov_inv=cov_inv, device=torch.device("cuda")
+    )
     return model
 
 
 def predict_image(image, path, thresh):
     """Turn image into batch, make prediction and return heatmap, score and threshold"""
     model = get_predict_model(path)
-    batch = anodet.to_batch([image], anodet.standard_image_transform, torch.device('cuda'))
+    batch = anodet.to_batch(
+        [image], anodet.standard_image_transform, torch.device("cuda")
+    )
     image_scores, score_maps = model.predict(batch)
     test_images = np.array([image]).copy()
-    heatmap_images = anodet.visualization.heatmap_images(test_images, score_maps, alpha=0.5)
+    heatmap_images = anodet.visualization.heatmap_images(
+        test_images, score_maps, alpha=0.5
+    )
     heatmap = cv2.cvtColor(heatmap_images[0], cv2.COLOR_BGR2RGB)
     return heatmap, round(image_scores[0].item(), 2)
--- pcb_anodet_server/update_projects.py
+++ pcb_anodet_server/update_projects.py
@@ -9,13 +9,23 @@ def update(project_name, settings):
 
 def update_crop(project_name, x_1, x_2, y_1, y_2):
     conf = load_conf(project_name)
-    conf["crop_config"]["crop_parameters"] = {"x_1": x_1, "x_2": x_2, "y_1": y_1, "y_2": y_2}
+    conf["crop_config"]["crop_parameters"] = {
+        "x_1": x_1,
+        "x_2": x_2,
+        "y_1": y_1,
+        "y_2": y_2,
+    }
     update(project_name, conf)
 
 
 def update_mask(project_name, x_1, x_2, y_1, y_2, color=255):
     conf = load_conf(project_name)
-    conf["mask_config"]["mask_parameters"] = {"x_1": x_1, "x_2": x_2, "y_1": y_1, "y_2": y_2}
+    conf["mask_config"]["mask_parameters"] = {
+        "x_1": x_1,
+        "x_2": x_2,
+        "y_1": y_1,
+        "y_2": y_2,
+    }
     conf["mask_config"]["mask_color"] = color
     update(project_name, conf)
 
