615aebce9e63f2a341971ba833d36496992b2682
VictorOlof <victorolofsson@hotmail.com>
2022-04-11 21:52:01 +0200

    Replaced js with flaskwtform

---
+++ pcb_anodet_server/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+
+def validate_project_name(form, field):
+    # TODO: Check if project name exist
+    # raise ValidationError('Project name already exists')
+    pass
+
+
+class ProjectForm(FlaskForm):
+    project_name = StringField("project_name", validators=[InputRequired(), Length(min=1, max=55), validate_project_name])
+    submit = SubmitField("Create project")
