2c61217717840eeb65dbc6215fed3b347168bead
VictorOlof <victorolofsson@hotmail.com>
2022-04-14 09:16:25 +0200

    Added device_type param to config

--- pcb_anodet_server/predict.py
+++ pcb_anodet_server/predict.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 import anodet
 import cv2
+from pcb_anodet_server.config import device_type
 
 
 def get_predict_model(path):
@@ -10,7 +11,7 @@ def get_predict_model(path):
     mean = torch.load(os.path.join(model_data_path, "mean.pt"))
     cov_inv = torch.load(os.path.join(model_data_path, "cov_inv.pt"))
     model = anodet.Padim(
-        backbone="resnet18", mean=mean, cov_inv=cov_inv, device=torch.device("cuda")
+        backbone="resnet18", mean=mean, cov_inv=cov_inv, device=torch.device(device_type)
     )
     return model
 
@@ -19,7 +20,7 @@ def predict_image(image, path, thresh):
     """Turn image into batch, make prediction and return heatmap, score and threshold"""
     model = get_predict_model(path)
     batch = anodet.to_batch(
-        [image], anodet.standard_image_transform, torch.device("cuda")
+        [image], anodet.standard_image_transform, torch.device(device_type)
     )
     image_scores, score_maps = model.predict(batch)
     test_images = np.array([image]).copy()
